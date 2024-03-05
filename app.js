8f06ebad5aa1a5ba918adc9c0746cd7dd84310d9
Muhammad Tayyab <786.mtayyab@gmail.com>
2024-03-05 22:36:01 +0500

    checked

---
+++ app.js
@@ -0,0 +1,325 @@
+
+
+//Chapt#4
+
+//Question#1
+    // var num1 = 5;
+    // var num2 = 3;
+    // var num3 = 4;
+
+    // alert(num1+num2*num3)
+
+    
+    //Question#2
+    // Leagle Variables Names
+
+    // var fullname = "Tayyab";
+    // alert(fullname);
+
+    // var fullName = "Ahmed";
+    // alert(fullName);
+
+    // var full_Name = "Ali";
+    // alert(full_Name);
+
+    // var fullName1 = "Asad";
+    // alert(fullName1)
+
+    // var FullName_1 = "Arif";
+    // alert(FullName_1);
+
+
+    // Illeagle Variables Names
+
+    // var alert = "Tayyab";
+    // document.write(alert);
+
+    // var Full Name = "Ahmed";
+    // alert(Full Name);
+
+    // var full-Name = "Ali";
+    // alert(full-Name);
+
+    // var 1fullName = "Asad";
+    // alert(1fullName)
+
+    // var 2-FullName = "Arif";
+    // alert(2-FullName);
+
+
+     //Question#3(a)
+    
+
+    // var head = "Rules for naming JS variables";
+    // document.write("<h1>"+head+"</h1>")
+
+    
+     //Question#3(b)
+    
+
+    // var B = ("Variable names can only contain, numbers, $ and underscores For example: $my_1stVariable");
+    // document.write(B);
+
+    //Question#3(c)
+    
+    // var C = ("Variable must begin with a letter, $ or underscores. For example: $name, _name or name");
+    // document.write(C);
+
+
+    //Question#3(d)
+
+    // var D = ("Variable names are case sensitive");
+    // document.write(D);
+
+
+    //Question#3(e)
+
+    // var E = ("Variable names should not be JS keywords");
+    // document.write(E);
+
+
+
+    //Chapt#5
+
+//Question# 1 (Addition)
+    // var num1 = 3;
+    // var num2 = 5;
+    // var sum = (num1+num2);
+    // document.write(sum);
+
+// Question# 2 (Substraction)
+
+    // var num1 = 3;
+    // var num2 = 5;
+    // var sub = (num1-num2);
+    // document.write(sub);
+   
+// Question# 2 (Multiplication)
+
+// var num1 = 3;
+// var num2 = 5;
+// var product = (num1*num2);
+// document.write(product);    
+   
+// Question# 2 (Multiplication)
+
+// var num1 = 3;
+// var num2 = 5;
+// var div = (num1/num2);
+// document.write(div); 
+
+// Question# 2 (Modulus)
+// var num1 = 5;
+// var num2 = 3;
+// var mod_result = (num1%num2);
+// document.write(mod_result);
+
+
+// Question# 3
+//     var num1 ;                                           
+//     document.write("<h1>"+num1+"</h1>");
+    
+//     num1 = "??";                                         
+//     document.write("<h1>"+"Value of variable after declaration is "+ num1 +"</h1>");
+
+//     var num1 = 5;                                       
+//     document.write("<h1>"+"Initial value of variable is "+num1 +"</h1>");
+    
+//     ++num1
+
+//    document.write("<h1>"+"Value of variable after the increament is "+ num1 +"</h1>");
+    
+//    var num2 = 7;
+//    var num3 = num1+num2;
+
+   
+
+//    document.write("<h1>"+"Value of variable after Addition of 7 in Increament "+(num3)+"</h1>");
+   
+//    --num3
+//    document.write("<h1>"+"Value of variable after Decreament "+(num3)+"</h1>");
+
+//    document.write("<h1>"+"Value of variable after divided by 3 the remainder is "+(num3 % 3)+"</h1>");
+   
+   
+
+   
+
+   
+
+// Question# 5
+// var num = 4;
+// var num1 = 1;
+
+
+// document.write(num + " x "+ num1 +" = "+  (num*num1) + "<br>");
+// ++num1
+// document.write(num + " x "+ num1 +" = "+  (num*num1) + "<br>");
+// ++num1
+// document.write(num + " x "+ num1 +" = "+  (num*num1) + "<br>");
+// ++num1
+// document.write(num + " x "+ num1 +" = "+  (num*num1) + "<br>");
+// ++num1
+// document.write(num + " x "+ num1 +" = "+  (num*num1) + "<br>");
+// ++num1
+// document.write(num + " x "+ num1 +" = "+  (num*num1) + "<br>");
+// ++num1
+// document.write(num + " x "+ num1 +" = "+  (num*num1) + "<br>");
+// ++num1
+// document.write(num + " x "+ num1 +" = "+  (num*num1) + "<br>");
+// ++num1
+// document.write(num + " x "+ num1 +" = "+  (num*num1) + "<br>");
+// ++num1
+// document.write(num + " x "+ num1 +" = "+  (num*num1) + "<br>");
+
+
+// Question# 6 
+// var temp_C = 25;
+
+// document.write(temp_C +" <sup>o</sup>C is "+((temp_C*9/5)+32) + " <sup>o</sup>F<br>");
+
+// var temp_F = 70;
+
+// document.write(temp_F +" <sup>o</sup>F is "+((temp_F-32)*5/9) + " <sup>o</sup>C");
+
+
+
+// Question# 7 
+
+// var priceItem1 = 650;
+// var quantityItem1 = 3;
+
+// var priceItem2 = 100;
+// var quantityItem2 = 7;
+
+// var shipCharges = 100;
+
+// document.write("Total Cost of Your Order Is " + ((priceItem1*quantityItem1)+(priceItem2*quantityItem2)+(shipCharges)) +" PKR");
+
+
+
+// Question# 8 
+
+// var totalMarks = 980;
+// var obtainedMarks = 804;
+
+
+// document.write("<h1>MARKS SHEET</h1><br><br><br>"+"Total Marks : "+totalMarks+"<br>"+"Obtained Marks : "+obtainedMarks+"<br>" +"Percentage : "+ obtainedMarks/totalMarks*100+" %")
+
+// Question# 9
+
+// var usDollars = 10;
+// var usExRate = 104.80;
+
+// var saRayals = 25;
+// var saExRate = 28;
+
+// document.write("<h1>Currency in PKR</h1><br><br><br>"+"Total Currency in PKR:" + (usDollars*usExRate + saRayals*saExRate));
+
+
+// Question# 10
+
+// var num1 = 7;
+// document.write(((num1+5)*7)/2);
+
+// Question# 11
+
+// var currentYear = 2016;
+// var birthYear = 1992;
+// var age = (currentYear-birthYear);
+   
+// document.write("<h1>Age Calculater</h1><br><br><br>" + "Current Year : " + currentYear + "<br>" + "Birth Year :" + birthYear + "<br>" + "Age : " + age);
+
+
+// Question# 12
+// var r_circle = 20;
+  
+// document.write("<h1>The Geometrizer</h1><br><br><br>" + "Radius of The Circle : " + r_circle + "<br>" + "The Circumfrance is :" +(2*3.142*20) + "<br>" + "The Area is :" +(3.142*(20**2) ));
+
+
+// Question# 13
+// var favour_snacks = "Chocklate Chip"; 
+// var currentAge = 15;
+// var maxAge = 65;
+// var amount_day = 3;
+ 
+  
+// document.write("<h1>The Life Time Supply Calculater</h1><br><br><br>" + "Favourite Snacks : " + favour_snacks + "<br>" + "Current Age :" +currentAge + "<br>" + "Maximum Age :" + maxAge + "<br>" + "Snacks Per Day : " + amount_day + "<br>"+" You Will need "+((maxAge-currentAge)*12*30*amount_day)+" Chocklate Chip to last You Until te Ripe age of 65 ");
+
+
+//Chapt# 6-9
+// Question# 1
+
+// var a = 10;
+// document.write("Reslult:<br>"+"Now the Value of a is " + a + "<br><br><br>")
+
+// a++
+// document.write("The Value of a++ is : " +a+ "<br>"+"Now the Value of a is : " + a + "<br><br><br>")
+
+// a++
+// document.write("The Value of a++ is : " +a+ "<br>"+"Now the Value of a is : " + a + "<br><br><br>")
+
+// a--
+// document.write("The Value of a-- is : " +a+ "<br>"+"Now the Value of a is : " + a + "<br><br><br>")
+
+// a--
+// document.write("The Value of a-- is : " +a+ "<br>"+"Now the Value of a is : " + a + "<br><br><br>")
+
+
+
+//Chapt# 6-9
+// Question# 2
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+
+
+// document.write(result);
+
+
+
+// Explain the output at each stage:
+//--a; value of a will be 1 due to pre decreament
+//--a - --b; value of b will be 0 due to pre decreament and substract from value of a which is now 1 (1-0 = 1)
+//--a - --b + ++b;  value of b will be 1 due to pre increament and add in previous result which is 1 (1+1=2) 
+//--a - --b + ++b + b--; value of b will be add in previous reslut which is 2 (2+1=3) due to post decreament 
+// final result will be 3
+
+
+//Chapt# 6-9
+// Question# 3 
+
+// var userName = prompt("enter Your Name", "Tayyab");
+// var greetingMsg = ("Welcome Tayyab");
+// console.write(greetingMsg);
+
+
+//Chapt# 6-9
+// Question# 5 (4 is missing) 
+
+// var num1 = prompt("Enter Required No" , "5");
+// var num2 = 1;
+
+// document.write (num1 + " x " + num2 + " = " + num1*num2 + "<br>");
+
+// ++num2
+// document.write (num1 + " x " + num2 + " = " + num1*num2 + "<br>");
+// ++num2
+// document.write (num1 + " x " + num2 + " = " + num1*num2 + "<br>");
+// ++num2
+// document.write (num1 + " x " + num2 + " = " + num1*num2 + "<br>");
+// ++num2
+// document.write (num1 + " x " + num2 + " = " + num1*num2 + "<br>");
+// ++num2
+// document.write (num1 + " x " + num2 + " = " + num1*num2 + "<br>");
+// ++num2
+// document.write (num1 + " x " + num2 + " = " + num1*num2 + "<br>");
+// ++num2
+// document.write (num1 + " x " + num2 + " = " + num1*num2 + "<br>");
+// ++num2
+// document.write (num1 + " x " + num2 + " = " + num1*num2 + "<br>");
+// ++num2
+// document.write (num1 + " x " + num2 + " = " + num1*num2 + "<br>");
+
+
+
+
